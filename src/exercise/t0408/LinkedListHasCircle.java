a1bc63f087e70d857ebbec6f94c39c53b6068d54
youyaa <listeningrain.cn@gmail.com>
2021-04-07 22:27:55 +0800

    04-08日练习题（链表）

---
+++ src/exercise/t0408/LinkedListHasCircle.java
@@ -0,0 +1,9 @@
+package exercise.t0408;
+
+/**
+ * Author: listeningrain
+ * Date: 2021/4/7 下午10:23
+ * Description:  判断链表是否有环（快慢指针）
+ */
+public class LinkedListHasCircle {
+}
---
+++ src/exercise/t0408/LinkedListReverse.java
@@ -0,0 +1,9 @@
+package exercise.t0408;
+
+/**
+ * Author: listeningrain
+ * Date: 2021/4/7 下午10:23
+ * Description:  链表翻转
+ */
+public class LinkedListReverse {
+}
