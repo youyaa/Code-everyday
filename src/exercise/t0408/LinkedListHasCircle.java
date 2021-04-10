98b2cd3528609129195aa987b3646a9e0e8f6d87
youyaa <listeningrain.cn@gmail.com>
2021-04-10 12:14:33 +0800

    完成04-08日题目

--- src/exercise/t0408/LinkedListHasCircle.java
+++ src/exercise/t0408/LinkedListHasCircle.java
@@ -6,4 +6,27 @@ package exercise.t0408;
  * Description:  判断链表是否有环（快慢指针）
  */
 public class LinkedListHasCircle {
+    //方法一：hash存储法
+
+    //方法二：快慢指针
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        if(null == head || head.next == null){
+            return false;
+        }
+
+        while (fast != null){
+            slow = slow.next;
+            if(fast.next != null){
+                fast = fast.next.next;
+            }else {
+                fast = null;
+            }
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/exercise/t0408/LinkedListReverse.java
+++ src/exercise/t0408/LinkedListReverse.java
@@ -6,4 +6,31 @@ package exercise.t0408;
  * Description:  链表翻转
  */
 public class LinkedListReverse {
+
+    public ListNode reverseList(ListNode head) {
+        if(null == head){
+            return head;
+        }
+        ListNode switchRightNode = head.next;
+        ListNode switchLeftNode = head;
+
+        head.next = null;
+
+        while(switchRightNode != null){
+            ListNode newHead = switchRightNode.next;
+            switchRightNode.next = switchLeftNode;
+            switchLeftNode = switchRightNode;
+            switchRightNode = newHead;
+        }
+        return switchLeftNode;
+    }
+}
+
+  //Definition for singly-linked list.
+  class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
